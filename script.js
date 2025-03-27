0a5796a7f0a94b94d4c2742da64479dab68aa45a
IzabelBacon <168193034+IzabelBacon@users.noreply.github.com>
2025-03-27 12:26:37 -0300

    pronto familia

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+function verificar() {
+    var numero = document.getElementById("numero").value;
+    var resultado = document.getElementById("resultado");
+
+    if (numero === "") {
+        resultado.innerHTML = "Por favor, insira um número.";
+        resultado.style.color = "yellow";
+    } else {
+        numero = parseInt(numero);
+        
+        if (numero % 2 === 0) {
+            resultado.innerHTML = "O número " + numero + " é Par.";
+            resultado.style.color = "#27ae60"; /* Verde para Par */
+        } else {
+            resultado.innerHTML = "O número " + numero + " é Ímpar.";
+            resultado.style.color = "#e74c3c"; /* Vermelho para Ímpar */
+        }
+    }
+}
